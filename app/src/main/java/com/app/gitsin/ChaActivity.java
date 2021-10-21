9082648d668a4e79c75410e64cedfe6a7ce12d70
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-10-21 19:17:13 +0900

    2021.10.21 19:20 oz

--- app/src/main/java/com/app/gitsin/ChaActivity.java
+++ app/src/main/java/com/app/gitsin/ChaActivity.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,13 +18,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class ChaActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton b1, b2, b3, b4, b5;
+    ImageView chaIVPerson, chaIVGroup;
     DatabaseReference database;
     User user;
+    ArrayList<ChaDTO> chaDTOs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,23 +36,52 @@ public class ChaActivity extends AppCompatActivity implements View.OnClickListen
 
         Intent intent = getIntent();
         user = (User)intent.getSerializableExtra("info");
+        final String TAG = "FIREBASE";
+
+        //현재 챌린지 참여 여부 확인해서 참여중인 경우 이미지 변경
+        if (user.getChaGroup() == 1){ chaIVGroup.setImageResource(R.drawable.cha_group); }
+        if (user.getChaPerson() == 1){ chaIVPerson.setImageResource(R.drawable.cha_personal); }
+
+        database = FirebaseDatabase.getInstance().getReference("challenge");
+        database.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //리스트 초기화
+                chaDTOs = new ArrayList<>();
+                //리스트 저장
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    ChaDTO a = data.getValue(ChaDTO.class);
+                    Log.d("파베 DB",a.toString());
+
+                    ChaDTO b = new ChaDTO(a.getCategory(), a.getLimit(), a.getName(), a.getParticipants(),a.getStartDate());
+                    Log.d("파베 DB",b.toString());
+
+                    boolean c = chaDTOs.add(b);
+                    Log.d("파베 DB2",chaDTOs.get(0).getCategory());
+                }
+
+                //DTO -> ChaListItem 데이터 가공
+                ArrayList<ChaListItem> chaListItems = new ArrayList<>();
+                Log.d("파베 어댑터", "데이터 가공 시작");
+                for (ChaDTO dto : chaDTOs) {
+                    //chaPart format: (String) 1/10
+                    int participants = (dto.getParticipants() != null) ? dto.getParticipants().split(",").length : 0;
+                    String chaPart = participants + "/" + dto.getLimit();
+
+                    chaListItems.add(new ChaListItem(dto.getName(),dto.getCategory(),dto.getStartDate(),chaPart));
+                }
+                Log.d("파베 어댑터", "데이터 가공 끝");
+
+                ListView listView = findViewById(R.id.chaListView);
+                ChaAdapter adapter = new ChaAdapter(chaListItems);
+                listView.setAdapter(adapter);
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) { }
+        });
 
 
-
-
-        // (일단) 데이터 정적 추가 -> DB에서 읽어와야 함
-        ChaListItem cha1 = new ChaListItem("호드", "단체전", "2021-10-24", "8/10");
-        ChaListItem cha2 = new ChaListItem("얼라이언스","단체전", "2021-10-25", "2/10");
-        ChaListItem cha3 = new ChaListItem("와우", "개인전", "2021-10-23", "1/2");
-
-        ArrayList<ChaListItem> data = new ArrayList<>();
-        data.add(cha1);
-        data.add(cha2);
-        data.add(cha3);
-
-        ListView listView = findViewById(R.id.chaListView);
-        ChaAdapter adapter = new ChaAdapter(data);
-        listView.setAdapter(adapter);
 
 
         //하단 메뉴
