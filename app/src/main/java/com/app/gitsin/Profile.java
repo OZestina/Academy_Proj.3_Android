4e118150e58cc93c8bdc9203c3d1ad131a3ef84e
junkyu lee <kkzz0001@gmail.com>
2021-10-24 17:50:46 +0900

    프로필에 오늘 업로드여부, 몇일째 업로드중인지 표시

--- app/src/main/java/com/app/gitsin/Profile.java
+++ app/src/main/java/com/app/gitsin/Profile.java
@@ -13,14 +13,13 @@ import android.widget.TextView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class profile extends AppCompatActivity implements View.OnClickListener {
+public class Profile extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton b1, b2, b3, b4, b5;
-    Button settingBtn;
-    DatabaseReference database;
-    TextView idView, signDateView, gitIdView;
+    TextView idView, signDateView, gitIdView, todayCheckMsg, streakCheckMsg;
     User user, user2;
     String key;
+    ImageView todayCheck;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,17 +39,23 @@ public class profile extends AppCompatActivity implements View.OnClickListener {
         idView = findViewById(R.id.idView2);
         gitIdView = findViewById(R.id.gitIdView2);
         signDateView = findViewById(R.id.signDateView2);
-
+        todayCheck = findViewById(R.id.todayCheck2);
+        todayCheckMsg = findViewById(R.id.todayCheckMsg2);
+        streakCheckMsg = findViewById(R.id.streakCheckMsg2);
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
         b3.setOnClickListener(this);
         b4.setOnClickListener(this);
         b5.setOnClickListener(this);
 
-        database = FirebaseDatabase.getInstance().getReference("users");
         idView.setText(user2.getUserId());
         gitIdView.setText(user2.getGithubId());
         signDateView.setText(user2.getSignDate() + "일에 가입");
+        if (user2.getTodayCount()==1){
+            todayCheck.setImageResource(android.R.drawable.presence_online);
+            todayCheckMsg.setVisibility(View.INVISIBLE);
+        }
+        streakCheckMsg.setText(user2.getStreakToday()+"일 연속 업로드중입니다.");
 
 
     }
@@ -60,19 +65,19 @@ public class profile extends AppCompatActivity implements View.OnClickListener {
         Intent intent;
         switch (view.getId()) {
             case R.id.menu7Pro :
-                intent = new Intent(profile.this, MainActivity.class);
+                intent = new Intent(Profile.this, MainActivity.class);
                 break;
             case R.id.menu7Hof :
-                intent = new Intent(profile.this, HoFActivity.class);
+                intent = new Intent(Profile.this, HoFActivity.class);
                 break;
             case R.id.menu7Stats :
-                intent = new Intent(profile.this, StatsActivity.class);
+                intent = new Intent(Profile.this, StatsActivity.class);
                 break;
             case R.id.menu7Friends :
-                intent = new Intent(profile.this, FriendsActivity.class);
+                intent = new Intent(Profile.this, FriendsActivity.class);
                 break;
             default :
-                intent = new Intent(profile.this, ChaActivity.class);
+                intent = new Intent(Profile.this, ChaActivity.class);
                 break;
         }
         intent.putExtra("info", user);
