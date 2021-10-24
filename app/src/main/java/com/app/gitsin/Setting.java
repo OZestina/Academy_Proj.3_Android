47917c33b19f207b39b6992762e50de50c1f9aaf
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-10-24 23:13:32 +0900

    2021.10.22 13:50 oz

--- app/src/main/java/com/app/gitsin/Setting.java
+++ app/src/main/java/com/app/gitsin/Setting.java
@@ -1,10 +1,13 @@
 package com.app.gitsin;
 
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.util.Log;
@@ -80,6 +83,13 @@ public class Setting extends PreferenceActivity {
             }
         });
 
+        //알람설정
+        final String ALARMID = "monaegiAlarm";
+        NotificationChannel gitsinAlarm = new NotificationChannel(ALARMID,"alarm channel", NotificationManager.IMPORTANCE_HIGH);
+
+                //createNotificationChannel(ALARMID,"alarm channel", NotificationManager.IMPORTANCE_HIGH);
+
+
         //다크모드 설정
         boolean dark = sp.getBoolean("darkMode",true);
         if (!dark) {
@@ -117,5 +127,23 @@ public class Setting extends PreferenceActivity {
             }
         });
 
-    }
+    } // end of onCreate
+
+//    private void createNotificationChannel() {
+//        // Create the NotificationChannel, but only on API 26+ because
+//        // the NotificationChannel class is new and not in the support library
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            CharSequence name = getString(R.string.channel_name);
+//            String description = getString(R.string.channel_description);
+//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+//            channel.setDescription(description);
+//            // Register the channel with the system; you can't change the importance
+//            // or other notification behaviors after this
+//            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//        }
+//    }
+
+
 }
